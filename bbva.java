e152791bf237b043cce4166e27b70471875574f7
jorge <brave240818.@gmail.com>
2024-11-16 10:40:18 -0600

    copia 2

---
+++ bbva.java
@@ -0,0 +1,23 @@
+class persona{
+    String Nombre;
+    String Sexo;
+    int Edad;
+
+    persona(String Nombre, String Sexo, String edad){
+        this.nombre = nombre;
+        this.sexo = sexo;
+        this.edad = edad;
+
+    }
+
+    
+}
+
+
+class programa{
+    public static void main(String¨[] args){
+        System.out.println("Hello word")
+    }
+
+
+}
